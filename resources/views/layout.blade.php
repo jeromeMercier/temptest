650bb11ce041b0e121c59a826b39fe757459624f
JeromeMercier <45977558+jeromeMercier@users.noreply.github.com>
2019-05-05 15:56:55 +0200

    Stable vuetify installation

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,17 +3,20 @@
 <head>
     <meta charset="utf-8">
     <title>Myjob · @yield('title', isset($title) ? $title: 'AGEPOLY')</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="format-detection" content="telephone=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons" rel="stylesheet">
     <link rel="shortcut icon" href="{{ asset('contents/images/favicon.png') }}">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 </head>
 <body>
+  <v-app id="app">
+  </v-app>
 
-<div class="ui container centered stackable grid content-wrapper">
-    @yield('content')
-</div>
-
+<script type="text/javascript" src="/js/app.js"></script>
 </body>
 </html>
